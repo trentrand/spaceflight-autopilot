b07ceb647afb890c63014c2d38b3087d23f3b35a
Trent Rand <contact@trentrand.com>
2020-09-12 23:53:36 -0400

    Setup 3D camera and render example SOI body using sphere primitive
    These changes setup a 3D camera for the window and render a sphere to
    test it out. Eventually this sphere will be dynamically sized based on
    information provided in headers of GPS output data format.

--- visualizer/main.c
+++ visualizer/main.c
@@ -7,13 +7,30 @@ int main(void) {
 
   InitWindow(screenWidth, screenHeight, "Automated Rocket Control Visualizer");
 
+  Camera camera = { 0 };
+  camera.position = (Vector3){ 800.0f, 800.0f, 800.0f };
+  camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+  camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+  camera.fovy = 60.0f;
+  camera.type = CAMERA_PERSPECTIVE;
+
+  SetCameraMode(camera, CAMERA_FIRST_PERSON);
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    UpdateCamera(&camera);
+
     BeginDrawing();
     {
       ClearBackground(RAYWHITE);
-      DrawText("Hello World", 300, 200, 20, LIGHTGRAY);
+      DrawText("Blue World", 250, 50, 20, BLUE);
+
+      BeginMode3D(camera);
+      {
+        DrawSphere((Vector3){ 0, 0, 0 }, 600.0f, BLUE);
+      }
+      EndMode3D();
     }
     EndDrawing();
   }
