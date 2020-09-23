1f3ec6ba0de729fc900e503b9af63b6f6518d844
Trent Rand <contact@trentrand.com>
2020-09-22 23:41:31 -0400

    Render positioned and scaled SOI and orbiting moon to match simulator
    Using the OpenGL scale has allowed the rendered scene positions to
    remain 1:1 with the simulator, but scaled in order to meet software
    rendering constraints. A ratio of 0.00001:1.0 has been applied between
    the visualizer and simulator respectively.
    
    With these improvements I was able to render the SOI and a new orbiting
    moon at their exact position and orbital apoapsis / periapsis. Work
    might be done later to accurately emulate the body and orbit rotational
    velocities.

--- visualizer/main.c
+++ visualizer/main.c
@@ -3,6 +3,7 @@
 #include "stdio.h"
 #include "string.h"
 #include "raylib.h"
+#include "rlgl.h"
 
 typedef struct GeocoordinateList {
   size_t length;
@@ -72,35 +73,65 @@ int main(int argc, char *argv[argc+1]) {
   strcpy(inputFilePath, argv[1]);
 
   GeocoordinateList *inputData;
-  parseInputData(inputFilePath, inputData);
+  parseInputData(inputFilePath, &inputData);
+  printf("Number of records parsed: %zu \n", inputData->length);
 
-  const int screenWidth = 600;
-  const int screenHeight = 600;
+  const int screenWidth = 1360;
+  const int screenHeight = 768;
 
   InitWindow(screenWidth, screenHeight, "Automated Rocket Control Visualizer");
 
+  const float visualizerScale = 0.00001f; // 0.00001 units in visualizer == 1 meter in simulator
+
+  const float earthRadius = 600000.0f;
+  const float moonRadius = 200000.0f;
+  const float moonOrbitRadius = 12000000.0f;
+  const float rotationSpeed = 0.1f;
+
+  float moonRotation = 0.0f;
+  float moonOrbitRotation = 0.0f;
+
   Camera camera = { 0 };
-  camera.position = (Vector3){ 800.0f, 800.0f, 800.0f };
+  const float cameraPosition = 100.0f;
+  camera.position = (Vector3){ cameraPosition, cameraPosition, cameraPosition };
   camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
   camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-  camera.fovy = 60.0f;
-  camera.type = CAMERA_PERSPECTIVE;
+  camera.fovy = 45.0f;
+  camera.type = CAMERA_PERSPECTIVE;;
 
-  SetCameraMode(camera, CAMERA_FIRST_PERSON);
+  SetCameraMode(camera, CAMERA_THIRD_PERSON);
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     UpdateCamera(&camera);
 
+    moonRotation += (5.0f * rotationSpeed);
+    moonOrbitRotation += (365 / 360.0f * (5.0f * rotationSpeed) * rotationSpeed);
+
     BeginDrawing();
     {
-      ClearBackground(RAYWHITE);
-      DrawText("Blue World", 250, 50, 20, BLUE);
+      ClearBackground((Color){ 52,60,64 });
 
       BeginMode3D(camera);
       {
-        DrawSphere((Vector3){ 0, 0, 0 }, 600.0f, BLUE);
+        rlScalef(visualizerScale, visualizerScale, visualizerScale);
+
+        DrawSphere((Vector3){ 0.0f, 0.0f, 0.0f }, earthRadius, BLUE);
+
+        rlPushMatrix();
+          rlRotatef(moonOrbitRotation, 0.0f, 1.0f, 0.0f);
+          rlTranslatef(moonOrbitRadius, 0.0f, 0.0f);
+          rlRotatef(-moonOrbitRotation, 0.0f, 1.0f, 0.0f);
+
+          rlPushMatrix();
+            rlRotatef(moonRotation, 0.25, 1.0, 0.0);
+            DrawSphere((Vector3){ 0.0f, 0.0f, 0.0f }, moonRadius, LIGHTGRAY);
+          rlPopMatrix();
+        rlPopMatrix();
+
+        DrawCircle3D((Vector3){ 0.0f, 0.0f, 0.0f }, moonOrbitRadius, (Vector3){ 1, 0, 0 }, 90.0f, Fade(WHITE, 0.15f));
+        DrawGrid(24, 1.0f);
       }
       EndMode3D();
     }
